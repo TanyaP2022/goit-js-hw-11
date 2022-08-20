0f11ad474f1cf17dad8c504af7e0a6a04ddcda01
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-20 23:48:05 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { fetchCountries } from './fetchCountries';
 const axios = require('axios').default;
 
 const DEBOUNCE_DELAY = 300;
