4dc46c80797b0d51889e338abc81269f3c12195b
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-21 16:57:18 +0300

    v9

---
+++ src/fetchPictures.js
@@ -0,0 +1,15 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const perPages = 40;
+const key = '29404582-ff8d29a0dc81dbac31eded24e';
+
+const fetchPictures = async (searchQuery, page) => {
+  const response = await axios.get(
+    `${BASE_URL}?key=${key}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPages}&page=${page}`
+  );
+  return response.data;
+};
+export default fetchPictures;
