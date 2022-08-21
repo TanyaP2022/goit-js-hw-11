8f6d21a87439effa173ab090bbb8e12d22c99fa0
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-21 17:17:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
